edd4841759d6292019ccc80fe5af2372c285e6d9
olibavictor <79920927+olibavictor@users.noreply.github.com>
2025-04-10 19:39:38 -0300

    Update custom-telegram.py

--- Wazuh/Integration/telegram/custom-telegram.py
+++ Wazuh/Integration/telegram/custom-telegram.py
@@ -20,20 +20,17 @@ alert_file.close()
 # Extract data fields
 alert_level = alert_json['rule']['level'] if 'level' in alert_json['rule'] else "N/A"
 title = alert_json['rule']['description'] if 'description' in alert_json['rule'] else "N/A"
-description = alert_json['full_log'] if 'full_log' else "N/A"
 groups = alert_json['rule']['groups'] if 'groups' in alert_json['rule'] else "N/A"
 rule_id = alert_json['rule']['id'] if 'id' in alert_json['rule'] else "N/A"
 agent = alert_json['agent']['name'] if 'name' in alert_json['agent'] else "N/A"
 
 # Generate request
-message = f"""🚨 *Alerta:* {title}
+message = f"""*Alerta:* {title}
 
 *Nível de Alerta:* {alert_level}
 *Agente:* {agent}
 *Regra:* {rule_id}
 *Grupo:* {groups}
-
-*Descrição:* {description}
 """
 
 msg_data = {
